8a2cb68f1104743132b4868ea79a77efd6683bf2
thanhnhan10 <nhanb1900295@student.ctu.edu.vn>
2022-04-05 22:52:06 +0700

    Tao trang them moi lien he

---
+++ src/views/ContactAdd.vue
@@ -0,0 +1,43 @@
+<template>
+    <div v-if="contact" class="page">
+        <h4>Thêm Liên hệ</h4>
+        <ContactForm
+            :contact="contact"
+            @submit:contact="createContact"
+        />
+        <p>{{ message }}</p>
+    </div>
+</template>
+
+<script>
+import ContactForm from "@/components/ContactForm.vue";
+import ContactService from "@/services/contact.service";
+
+export default {
+    components: {
+        ContactForm,
+    },
+    data() {
+        return {
+            contact: {
+                name: "",
+                email: "",
+                address: "",
+                phone: "",
+                favorite: false
+            },
+            message: "",
+        };
+    },
+    methods: {
+        async createContact(data) {
+            try {
+                await ContactService.create(data);
+                this.message = "Liên hệ được tạo thành công.";
+            } catch (error) {
+                console.log(error);
+            }
+        },
+    },
+};
+</script>
